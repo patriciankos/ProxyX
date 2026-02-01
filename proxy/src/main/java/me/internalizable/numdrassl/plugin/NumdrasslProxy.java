6a80d818e3a75647b55de9b05404e13d227270ad
PPPPP <dev@auradev.pro>
2026-02-01 22:21:03 +0100

    updated commands

--- proxy/src/main/java/me/internalizable/numdrassl/plugin/NumdrasslProxy.java
+++ proxy/src/main/java/me/internalizable/numdrassl/plugin/NumdrasslProxy.java
@@ -152,14 +152,14 @@ public final class NumdrasslProxy implements ProxyServer {
     }
 
     private void registerBuiltinCommands() {
-        commandManager.register(this, new HelpCommand(commandManager));
+        //commandManager.register(this, new HelpCommand(commandManager));
         commandManager.register(this, new AuthCommand(core));
-        commandManager.register(this, new SessionsCommand(core));
-        commandManager.register(this, new StopCommand(core), "shutdown", "end");
-        commandManager.register(this, new ServerCommand(), "srv");
-        commandManager.register(this, new FindCommand(), "find-server");
-        commandManager.register(this, new NumdrasslCommand(), "nd", "proxy");
-        commandManager.register(this, new MetricsCommand(), "stats", "perf", "performance");
+        //commandManager.register(this, new SessionsCommand(core));
+        commandManager.register(this, new StopCommand(core), "end");
+        //commandManager.register(this, new ServerCommand(), "srv");
+        //commandManager.register(this, new FindCommand(), "find-server");
+        //commandManager.register(this, new NumdrasslCommand(), "nd", "proxy");
+        //commandManager.register(this, new MetricsCommand(), "stats", "perf", "performance");
     }
 
     private void registerCommandListener() {
